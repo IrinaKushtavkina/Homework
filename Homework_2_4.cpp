2ed8a79456cfd1be0df0d8dcedb4c0166dcdaca1
Ирина <tsukishir0@mail.ru>
2021-02-05 00:45:51 +0300

    Homework_2_1.cpp

---
+++ Homework_2_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+struct Tic_tac_toe_game
+{
+int Board [3] [3] = {{}, {}};
+char Signs [] = {' ', 'X', 'O', '\0'};
+};
+
+return 0;
+}
